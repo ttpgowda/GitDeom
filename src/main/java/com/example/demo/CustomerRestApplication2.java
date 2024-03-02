f7a99ce9454f59f4cbcc157c9e25331afd1cfa26
Thimmegowda <thimmegowdatp@gamil.com>
2024-03-02 13:08:30 +0530

    Comniting 2 new File to Brach X

---
+++ src/main/java/com/example/demo/CustomerRestApplication2.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRestApplication2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerRestApplication2.class, args);
+	}
+
+}
