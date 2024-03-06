e825d0d5e51ce54bfbe4d3d4602acc42cec63ffb
Thimmegowda <thimmegowdatp@gamil.com>
2024-03-07 00:42:33 +0530

    this is modified

--- src/main/java/com/example/demo/CustomerRestApplication.java
+++ src/main/java/com/example/demo/CustomerRestApplication.java
@@ -8,6 +8,12 @@ public class CustomerRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerRestApplication.class, args);
+		
+		System.out.println(add(10,20));
+	}
+	
+	static int  add(int a, int b) {
+		return a+b;
 	}
 
 }
